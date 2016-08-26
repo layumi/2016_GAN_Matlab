e3360a6d7fa439913a82616a0fb638ad341a5fb5
Layumi <12307130083@fudan.edu.cn>
2016-08-26 20:50:37 +0800

    1.0

--- train_baseline.m
+++ train_baseline.m
@@ -1,13 +1,13 @@
 function train_baseline(varargin)
 
 % Load character dataset
-imdb = load('./scale_minist.mat') ;
+imdb = load('./minist_data.mat') ;
 imdb = imdb.imdb;
 % -------------------------------------------------------------------------
 % Part 4.2: initialize a CNN architecture
 % -------------------------------------------------------------------------
 
-net = resnet52_bs();
+net = GDnet();
 net.conserveMemory = true;
 
 
@@ -15,15 +15,16 @@ net.conserveMemory = true;
 % Part 4.3: train and evaluate the CNN
 % -------------------------------------------------------------------------
 opts.train.averageImage = mean(imdb.images.data_mean(:));
-opts.train.batchSize = 36;
+opts.train.batchSize = 2;
 %opts.train.numSubBatches = 1 ;
 opts.train.continue = true; 
 opts.train.gpus = 3;
 opts.train.prefetch = false ;
 %opts.train.sync = false ;
 %opts.train.errorFunction = 'multiclass' ;
-opts.train.expDir = '/data/resnet_baseline' ;
+opts.train.expDir = '/data/GAN' ;
 opts.train.learningRate = [0.1*ones(1,100),0.01*ones(1,20)] ;
+opts.train.derOutputs = {'objective', 1} ;
 opts.train.weightDecay = 0.0005;
 opts.train.numEpochs = numel(opts.train.learningRate) ;
 [opts, ~] = vl_argparse(opts.train, varargin) ;
@@ -34,18 +35,9 @@ opts.train.numEpochs = numel(opts.train.learningRate) ;
 % --------------------------------------------------------------------
 function inputs = getBatch(imdb, batch,opts)
 im = imdb.images.data(:,:,:,batch);
-labels = imdb.images.label(:,batch);
-batchsize = size(im,4);
-im = bsxfun(@minus,im,opts.averageImage);
-oim = zeros(227,227,3,batchsize,'single');
-for i =1:batchsize
-    x = randi(29);
-    y = randi(29);
-    temp = im(x:x+226,y:y+226,:,i);
-    r = rand>0.5;
-    if r
-        oim(:,:,:,i) = temp;
-    else oim(:,:,:,i) = filplr(temp);
-    end
-end
-inputs = {'x0',gpuArray(oim),'label',labels};
+batchsize = numel(batch);
+half = round(batchsize/2);
+labels = [ones(1,half,'single'),2*ones(1,batchsize-half,'single')];  % 1 for data_rand     2 for data_gt
+im_gt = bxsfun(@minus,im(:,:,:,batchsize-half),opts.train.averageImage);
+im_rand = rand(28,28,1,half);
+inputs = {'data_rand',gpuArray(im_rand),'data_gt',gpuArray(im_gt),'label',labels};
